4a6e8ad50c8072e8f0c04f5a50d128b02db04abc
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-11-25 02:03:02 +0400

    Product Inventory Management System Application

---
+++ src/test/java/com/Inventory/Management/System/demo/ProductInventoryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Inventory.Management.System.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductInventoryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
